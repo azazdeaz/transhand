568581965804985835a5085a46233e5d20a9e18b
Polgar <azazdeaz@gmail.com>
2015-06-05 07:53:41 +0200

    demo

--- demo/src/prepare.js
+++ demo/src/prepare.js
@@ -1,4 +1,13 @@
 require.context('./assets', false, /\.png$/);
+var random = require('lodash/number/random');
+var clone = require('lodash/lang/clone');
+const INIT_PARAMS = {
+  tx: 0, ty: 0,
+  sx: 1, sy: 1,
+  rz: 0,
+  ox: 0.5, oy: 0.5,
+};
+const IMG_SIZE = 96;
 
 export default function scatterThings() {
 
@@ -9,38 +18,39 @@ export default function scatterThings() {
 
   var rootNode = document.querySelector('#stuffs');
 
-  // for (var i = 0; i < 12; ++i) {
-  //
-  //   var div = document.createElement('div');
-  //   div.style.width = (32 + 132 * Math.random()) + 'px';
-  //   div.style.height = (32 + 132 * Math.random()) + 'px';
-  //   div.style.background = '#' + Math.random().toString(16).substr(-3);
-  //   div.style.boxShadow = '1px 1px 4px 0px rgba(50, 50, 50, 0.75)';
-  //
-  //   place(div);
-  //
-  //   div._handlerDemo = 'boxer';
-  //
-  //   div._handlerParams = {
-  //       x: div.offsetLeft,
-  //       y: div.offsetTop,
-  //       w: div.offsetWidth,
-  //       h: div.offsetHeight,
-  //   };
-  // }
+  for (let i = 0; i < 3; ++i) {
 
-  srcs.forEach(function (src) {
+    var div = document.createElement('div');
+    div.style.width = (232 + 132 * Math.random()) + 'px';
+    div.style.height = (232 + 132 * Math.random()) + 'px';
+    div.style.background = '#' + Math.random().toString(16).substr(-3);
+    div.style.boxShadow = '1px 1px 4px 0px rgba(50, 50, 50, 0.75)';
 
+    div._handlerParams = clone(INIT_PARAMS);
+
+    place(div, rootNode);
+
+    div._handlerDemo = 'boxer';
+
+    div._handlerBase = {
+        x: div.offsetLeft,
+        y: div.offsetTop,
+        w: div.offsetWidth,
+        h: div.offsetHeight,
+    };
+
+    for (let j = 0; j < 3; ++j) {
+      let img = createImg(srcs[random(srcs.length - 1)]);
+      place(img, div);
+    }
+  }
+
+function createImg(src) {
     var img = new Image();
 
     img._handlerDemo = 'transformer';
 
-    img._handlerParams = {
-      tx: 0, ty: 0,
-      sx: 1, sy: 1,
-      rz: 0,
-      ox: 0.5, oy: 0.5,
-    };
+    img._handlerParams = clone(INIT_PARAMS);
 
     img.onload = function () {
 
@@ -64,17 +74,20 @@ export default function scatterThings() {
       };
     };
 
-    place(img);
-
     img.src = src;
-  });
 
-  function place(de) {
+    return img;
+  }
 
-    de.style.left = parseInt((window.innerWidth - 150) * Math.random()) + 'px';
-    de.style.top = parseInt((window.innerHeight - 150) * Math.random()) + 'px';
+  function place(de, deParent) {
+    deParent.appendChild(de);
+
+    var w = deParent.offsetWidth - (de.offsetWidth || IMG_SIZE);
+    var h = deParent.offsetHeight - (de.offsetHeight || IMG_SIZE);
+
+    de.style.left = parseInt(w * Math.random()) + 'px';
+    de.style.top = parseInt(h * Math.random()) + 'px';
     de.style.position = 'absolute';
     de.style.cursor = 'pointer';
-    rootNode.appendChild(de);
   }
 }
