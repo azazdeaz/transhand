96e39f88af63fe3901f46260cac4e0e00b9a4730
Polgár András <azazdeaz@gmail.com>
2015-10-15 01:48:40 +0200

    remove react-dom dependency to support react v0.13

--- src/CSSCoordinator.js
+++ src/CSSCoordinator.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react'
 import heuristicGlobalToLocal from './heuristicGlobalToLocal'
 import getGlobalBoundingClientRect from './getGlobalBoundingClientRect'
 import findWhere from 'lodash/collection/findWhere'
--- src/cursorHint/CursorHint.js
+++ src/cursorHint/CursorHint.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react'
 import CursorHintDesign from './CursorHintDesign'
 
 export default class CursorHint extends React.Component {
